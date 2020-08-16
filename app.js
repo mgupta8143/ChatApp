acf805dda4986cecd1a5a3c454fe80d96035bc25
mgupta8143 <mgupta8143@gmail.com>
2020-08-16 16:17:22 -0400

    Changed Port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const socketio = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
